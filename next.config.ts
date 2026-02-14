70bc8cf48115804b2a3c35bd7c8e0e5e286f72c6
smartave99 <smartave99@gmail.com>
2026-02-14 11:43:26 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -133,6 +133,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
